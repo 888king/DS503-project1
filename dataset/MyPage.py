db704cca2da45e7c68b6164063042ee18cf2061f
888king <m13001278911@163.com>
2020-02-06 19:31:18 -0500

    update

--- dataset/MyPage.py
+++ dataset/MyPage.py
@@ -3,9 +3,32 @@ import random
 import pandas
 import pycountry
 
-n = 2000
+n = 200
+random.seed(10)
+hobby_list = pandas.read_csv('hobbies.csv')['hobby'].tolist()
+hobby_length = len(hobby_list)
+country_length = len(pycountry.countries)
 
-people = list()
+people = []
+countries = []
+codes = []
+hobbies = []
+for i in range(n):
+    people.append(names.get_full_name())
+    code = random.randint(1, country_length)
+    codes.append(code)
+    countries.append(list(pycountry.countries)[code-1].name)
+    hobbies.append(hobby_list[random.randint(0, hobby_length-1)])
+MyPage = pandas.DataFrame({
+    'ID': range(1, n+1),
+    'Name': people,
+    'Nationality': countries,
+    'CountryCode': codes,
+    'Hobby': hobbies
+})
+print(MyPage['Nationality'])
+
+'''people = list()
 for x in range(1, n):
   people.append(names.get_full_name()[:20])
 
@@ -37,4 +60,4 @@ MyPage = pandas.DataFrame({
     'Nationality': people_country,
     'Country_Code': count_code,
     'Hobby': people_hobby})
-MyPage.to_csv('./MyPage.csv')
+MyPage.to_csv('./MyPage.csv')'''
